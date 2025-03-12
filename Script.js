737eb68469354ce288d953fe55ca30d9f73a8dc0
Ivan127945 <vany100105@gmail.com>
2025-03-12 12:01:07 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {});
+
+const leftImage = document.getElementById("leftImage");
+
+
+leftImage.addEventListener("click", function() {
+   
+    window.location.href = "https://warthunder.com/en";
+});
+
+const rightImage = document.getElementById("rightImage");
+rightImage.addEventListener("click", function(){
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+});
